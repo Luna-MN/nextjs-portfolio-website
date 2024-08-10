b73af890a74237bee4f0aa96e714ef29774f5422
Matthew Nock <thematt.nock@gmail.com>
2024-08-10 22:41:58 +0100

    more links to tools that i used into the projects

--- app/projects/FF14DiscordBot/page.tsx
+++ app/projects/FF14DiscordBot/page.tsx
@@ -29,58 +29,93 @@ export default async function DutyCompletePage() {
                     given <br /> item name. <br /> /itemData will give you the
                     locations to get an item based on the item ID. this is an
                     easy way to get what quest/NPC you need to <br /> go to. it
-                    does this but using the XIVDB api and searching for the item
-                    this will give you a long list of accosiated ID's and from
-                    this <br /> we can sanitize the data into places you get the
-                    item from this and then return it to the user.
+                    does this but using the Garland Tools api and searching for
+                    the item this will give you a long list of accosiated ID's
+                    and from this <br /> we can sanitize the data into places
+                    you get the item from this and then return it to the user.
                 </p>
                 <div className="flex justify-center items-center w-full gap-8 mx-auto mt-32 sm:mt-0 lg:gap-16 pt-10">
-                    <Card>
-                        <Link
-                            href={'https://github.com/FabianXh/FF14DiscordBot'}
-                            target="_blank"
-                            className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
-                        >
-                            <span
-                                className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-                                aria-hidden="true"
-                            />
-                            <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-                                {<Github size={20} />}
-                            </span>{' '}
-                            <div className="z-10 flex flex-col items-center">
-                                <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
-                                    {'FF14 Discord Bot'}
+                    <div className="sm:grid-cols-2">
+                        <h2 className="flex justify-center items-center text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl pb-4">
+                            Github Repo
+                        </h2>
+                        <Card>
+                            <Link
+                                href={
+                                    'https://github.com/FabianXh/FF14DiscordBot'
+                                }
+                                target="_blank"
+                                className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+                            >
+                                <span
+                                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                                    aria-hidden="true"
+                                />
+                                <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                                    {<Github size={20} />}
+                                </span>{' '}
+                                <div className="z-10 flex flex-col items-center">
+                                    <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+                                        {'FF14 Discord Bot'}
+                                    </span>
+                                    <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                                        {'Github'}
+                                    </span>
+                                </div>
+                            </Link>
+                        </Card>
+                    </div>
+                    <div>
+                        <h2 className="flex justify-center items-center text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl pb-4">
+                            API Docs
+                        </h2>
+                        <Card>
+                            <Link
+                                href={'https://discord.js.org'}
+                                target="_blank"
+                                className="p-1 relative flex flex-col items-center gap-2 duration-700 group md:gap-2 md:py-7 lg:pb-24 md:p-8"
+                            >
+                                <span
+                                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                                    aria-hidden="true"
+                                />
+                                <span className="relative z-10 flex items-center justify-center w-8 h-8 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                                    {<ArrowUpIcon size={20} />}
+                                </span>{' '}
+                                <div className="z-10 flex flex-col items-center">
+                                    <span className="lg:text-lg font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
+                                        {'Discord.js'}
+                                    </span>
+                                    <span className="mt-2 text-xs text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                                        {'API Docs'}
+                                    </span>
+                                </div>
+                            </Link>
+                        </Card>
+                        <Card>
+                            <Link
+                                href={'https://garlandtools.org/db/'}
+                                target="_blank"
+                                className="p-1 relative flex flex-col items-center gap-2 duration-700 group md:gap-2 md:py-7 lg:pb-24 md:p-8"
+                            >
+                                <span
+                                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                                    aria-hidden="true"
+                                />
+                                <span className="relative z-10 flex items-center justify-center w-8 h-8 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                                    {<ArrowUpIcon size={16} />}
                                 </span>
-                                <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                                    {'Github'}
-                                </span>
-                            </div>
-                        </Link>
-                    </Card>
-                    <Card>
-                        <Link
-                            href={'https://discord.js.org'}
-                            target="_blank"
-                            className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
-                        >
-                            <span
-                                className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-                                aria-hidden="true"
-                            />
-                            <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-                                {<ArrowUpIcon size={20} />}
-                            </span>{' '}
-                            <div className="z-10 flex flex-col items-center">
-                                <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
-                                    {'Discord.js'}
-                                </span>
-                                <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                                    {'API Docs'}
-                                </span>
-                            </div>
-                        </Link>
-                    </Card>
+                                <div className="z-10 flex flex-col items-center">
+                                    <span className="lg:text-lg font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
+                                        {'Garland Tools'}
+                                    </span>
+                                    <span className="mt-2 text-xs text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                                        {'Garland Tools'}
+                                    </span>
+                                </div>
+                            </Link>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </div>
--- app/projects/dutycomplete/page.tsx
+++ app/projects/dutycomplete/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigation } from '../../components/nav';
 import { Card } from '../../components/card';
-import { Github } from 'lucide-react';
+import { Github, ArrowUpIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export default async function DutyCompletePage() {
@@ -28,30 +28,87 @@ export default async function DutyCompletePage() {
                     then logging the kill time and increasing the kill count by
                     one.
                 </p>
-                <div className="flex justify-center items-center w-full gap-8 mx-auto mt-32 sm:mt-0 lg:gap-16 pt-10">
-                    <Card>
-                        <Link
-                            href={'https://github.com/Luna-MN/ffxiv-plugin'}
-                            target="_blank"
-                            className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
-                        >
-                            <span
-                                className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-                                aria-hidden="true"
-                            />
-                            <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-                                {<Github size={20} />}
-                            </span>{' '}
-                            <div className="z-10 flex flex-col items-center">
-                                <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
-                                    {'Duty Complete Plugin'}
+                <div className="flex justify-center items-center text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl pt-8"></div>
+                <div className="flex justify-center items-center w-full gap-8 mx-auto mt-32 sm:mt-0 lg:gap-16 pt-8">
+                    <div className="sm:grid-cols-2">
+                        <h2 className="flex justify-center items-center text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl pb-4">
+                            Github Repo
+                        </h2>
+                        <Card>
+                            <Link
+                                href={'https://github.com/Luna-MN/ffxiv-plugin'}
+                                target="_blank"
+                                className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+                            >
+                                <span
+                                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                                    aria-hidden="true"
+                                />
+                                <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                                    {<Github size={20} />}
+                                </span>{' '}
+                                <div className="z-10 flex flex-col items-center">
+                                    <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+                                        {'Duty Complete Plugin'}
+                                    </span>
+                                    <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                                        {'Github'}
+                                    </span>
+                                </div>
+                            </Link>
+                        </Card>
+                    </div>
+                    <div>
+                        <h2 className="flex justify-center items-center text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl pb-4">
+                            API Docs
+                        </h2>
+                        <Card>
+                            <Link
+                                href={'https://goatcorp.github.io/faq/'}
+                                target="_blank"
+                                className="p-1 relative flex flex-col items-center gap-2 duration-700 group md:gap-2 md:py-7 lg:pb-24 md:p-8"
+                            >
+                                <span
+                                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                                    aria-hidden="true"
+                                />
+                                <span className="relative z-10 flex items-center justify-center w-8 h-8 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                                    {<ArrowUpIcon size={16} />}
                                 </span>
-                                <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                                    {'Github'}
+                                <div className="z-10 flex flex-col items-center">
+                                    <span className="lg:text-lg font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
+                                        {'XIV Quick Launcher'}
+                                    </span>
+                                    <span className="mt-2 text-xs text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                                        {'Dalamud'}
+                                    </span>
+                                </div>
+                            </Link>
+                        </Card>
+                        <Card>
+                            <Link
+                                href={'https://www.finalfantasyxiv.com'}
+                                target="_blank"
+                                className="p-1 relative flex flex-col items-center gap-2 duration-700 group md:gap-2 md:py-7 lg:pb-24 md:p-8"
+                            >
+                                <span
+                                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                                    aria-hidden="true"
+                                />
+                                <span className="relative z-10 flex items-center justify-center w-8 h-8 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                                    {<ArrowUpIcon size={16} />}
                                 </span>
-                            </div>
-                        </Link>
-                    </Card>
+                                <div className="z-10 flex flex-col items-center">
+                                    <span className="lg:text-lg font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
+                                        {'Final Fantasy XIV'}
+                                    </span>
+                                    <span className="mt-2 text-xs text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                                        {'FFXIV'}
+                                    </span>
+                                </div>
+                            </Link>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </div>
